bd088883802638958f75a8d2de8e570e42ce8e47
prudhvi <prudhvikarimeti18@gmail.com>
2023-06-13 10:43:41 +0000

    rpspractice

---
+++ src/components/ScoreView/index.js
@@ -0,0 +1,30 @@
+import {
+  ScoreContainer,
+  ScoreNameContainer,
+  ScoreName,
+  ScoreBoard,
+  ScoreResult,
+  ScoreHeading,
+} from './styledComponents'
+
+const ScoreView = props => {
+  const {score} = props
+  return (
+    <ScoreContainer>
+      <ScoreNameContainer>
+        <ScoreName>
+          ROCK
+          <br />
+          PAPER
+          <br />
+          SCISSORS
+        </ScoreName>
+      </ScoreNameContainer>
+      <ScoreBoard>
+        <ScoreHeading>Score</ScoreHeading>
+        <ScoreResult>{score}</ScoreResult>
+      </ScoreBoard>
+    </ScoreContainer>
+  )
+}
+export default ScoreView
